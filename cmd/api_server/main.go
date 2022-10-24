a34323694ebaf678f353c6421c68a98f67b0050a
Lyalyashechka <Rudukh.Eugene@yandex.ru>
2022-10-25 00:38:45 +0300

    hello

--- cmd/api_server/main.go
+++ cmd/api_server/main.go
@@ -7,8 +7,8 @@ import (
 
 func main() {
 	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
+	e.GET("/hello", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
-	e.Logger.Fatal(e.Start("localhost:8080"))
+	e.Logger.Fatal(e.Start("localhost:8091"))
 }
