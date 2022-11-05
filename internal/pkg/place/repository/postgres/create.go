3761ba78d81c44b3f4e527ec9d7788e64ad89dbe
Lyalyashechka <Rudukh.Eugene@yandex.ru>
2022-11-05 11:42:33 +0300

    new db

--- internal/pkg/place/repository/postgres/create.go
+++ internal/pkg/place/repository/postgres/create.go
@@ -2,37 +2,71 @@ package postgres
 
 import (
 	"context"
+	"encoding/json"
 	"github.com/Lyalyashechka/VDNX/internal/pkg/models"
 	db_models "github.com/Lyalyashechka/VDNX/internal/pkg/postgres/models"
 	"github.com/pkg/errors"
 	"gorm.io/gorm"
+	"sort"
 	"strconv"
 )
 
 func (p *Postgres) Create(ctx context.Context, place models.Place) error {
 	err := p.db.Transaction(func(tx *gorm.DB) error {
+		jsonWorkShedule, err := json.Marshal(place.WorkSchedule)
+		if err != nil {
+			p.logger.WithError(err).Errorf("[Create] error marshal")
+			return err
+		}
+		sort.Sort(sort.Reverse(sort.Float64Slice(place.Coordinates)))
 		dbPlace := db_models.Place{
-			Id:          place.Id,
-			Qr:          place.Qr,
-			Order:       place.Order,
-			Coordinates: place.Coordinates,
-			Facilities:  place.Facilities,
-			VideoRoute:  place.VideoRoute,
-			TicketsLink: place.TicketsLink,
-			TicketsText: place.TicketsText,
-			Cat:         place.Cat,
-			Visibility:  place.Visibility,
-			Color:       place.Color,
-			ColorCode:   place.ColorCode,
-			PreviewText: place.PreviewText,
-			DetailText:  place.DetailText,
-			Title:       place.Title,
-			Time:        place.Time,
-			Type:        place.Type,
-			Url:         place.Url,
-			Pic:         place.Pic,
-			Code:        place.Code,
-			IsEvent:     place.IsEvent,
+			Id:               place.Id,
+			Qr:               place.Qr,
+			Order:            place.Order,
+			Coordinates:      place.Coordinates,
+			Facilities:       place.Facilities,
+			VideoRoute:       place.VideoRoute,
+			TicketsLink:      place.TicketsLink,
+			TicketsText:      place.TicketsText,
+			Cat:              place.Cat,
+			Visibility:       place.Visibility,
+			Color:            place.Color,
+			ColorCode:        place.ColorCode,
+			PreviewText:      place.PreviewText,
+			DetailText:       place.DetailText,
+			Title:            place.Title,
+			Time:             place.Time,
+			Type:             place.Type,
+			Url:              place.Url,
+			Pic:              place.Pic,
+			Code:             place.Code,
+			IsEvent:          place.IsEvent,
+			ElectrobusFlag:   place.ElectrobusFlag,
+			ScooterFlag:      place.ScooterFlag,
+			Free:             place.Free,
+			Rating:           place.Rating,
+			ChildrenFlag:     place.ChildrenFlag,
+			DateFlag:         place.DateFlag,
+			FamilyFlag:       place.FamilyFlag,
+			AnimalsFlag:      place.AnimalsFlag,
+			BikeFlag:         place.BikeFlag,
+			Promenade:        place.Promenade,
+			Activity:         place.Activity,
+			Nature:           place.Nature,
+			Science:          place.Science,
+			National:         place.National,
+			Workshop:         place.Workshop,
+			Creation:         place.Creation,
+			Kids:             place.Kids,
+			Tech:             place.Tech,
+			AboutRussia:      place.AboutRussia,
+			TypeService:      place.TypeService,
+			Duration:         place.Duration,
+			ProcessedPreview: place.ProcessedPreview,
+			ProcessedTitle:   place.ProcessedTitle,
+			VectorText:       place.VectorText,
+			VectorTitle:      place.VectorTitle,
+			WorkSchedule:     string(jsonWorkShedule),
 		}
 
 		res := p.db.Table("places").Create(&dbPlace)
--- internal/pkg/postgres/models/place.go
+++ internal/pkg/postgres/models/place.go
@@ -3,27 +3,54 @@ package models
 import "github.com/lib/pq"
 
 type Place struct {
-	Id          int             `gorm:"column:id"`
-	Qr          string          `gorm:"column:qr"`
-	Order       int             `gorm:"column:order_place"`
-	Coordinates pq.Float64Array `gorm:"column:coordinates"`
-	Facilities  pq.StringArray  `gorm:"column:facilities"`
-	VideoRoute  string          `gorm:"column:video_route"`
-	TicketsLink string          `gorm:"column:tickets_link"`
-	TicketsText string          `gorm:"column:tickets_text"`
-	Cat         string          `gorm:"column:cat"`
-	Visibility  string          `gorm:"column:visibility"`
-	Color       string          `gorm:"column:color"`
-	ColorCode   string          `gorm:"column:color_code"`
-	PreviewText string          `gorm:"column:preview_text"`
-	DetailText  string          `gorm:"column:detail_text"`
-	Title       string          `gorm:"column:title"`
-	Time        string          `gorm:"column:time_work"`
-	Type        string          `gorm:"column:type_place"`
-	Url         string          `gorm:"column:url"`
-	Pic         string          `gorm:"column:pic"`
-	Code        string          `gorm:"column:code"`
-	IsEvent     bool            `gorm:"column:is_event"`
+	Id               int             `gorm:"column:id"`
+	Qr               string          `gorm:"column:qr"`
+	Order            int             `gorm:"column:order_place"`
+	Coordinates      pq.Float64Array `gorm:"column:coordinates"`
+	Facilities       pq.StringArray  `gorm:"column:facilities"`
+	VideoRoute       string          `gorm:"column:video_route"`
+	TicketsLink      string          `gorm:"column:tickets_link"`
+	TicketsText      string          `gorm:"column:tickets_text"`
+	Cat              string          `gorm:"column:cat"`
+	Visibility       string          `gorm:"column:visibility"`
+	Color            string          `gorm:"column:color"`
+	ColorCode        string          `gorm:"column:color_code"`
+	PreviewText      string          `gorm:"column:preview_text"`
+	DetailText       string          `gorm:"column:detail_text"`
+	Title            string          `gorm:"column:title"`
+	Time             string          `gorm:"column:time_work"`
+	Type             string          `gorm:"column:type_place"`
+	Url              string          `gorm:"column:url"`
+	Pic              string          `gorm:"column:pic"`
+	Code             string          `gorm:"column:code"`
+	ElectrobusFlag   int             `gorm:"column:electrobus_flag"`
+	ScooterFlag      int             `gorm:"column:scooter_flag"`
+	Free             int             `gorm:"column:free"`
+	Rating           int64           `gorm:"column:rating"`
+	ChildrenFlag     int             `gorm:"column:children_flag"`
+	DateFlag         int             `gorm:"column:date_flag"`
+	FamilyFlag       int             `gorm:"column:family_flag"`
+	AnimalsFlag      int             `gorm:"column:animals_flag"`
+	BikeFlag         int             `gorm:"column:bike_flag"`
+	Promenade        int             `gorm:"column:promenade"`
+	Activity         int             `gorm:"column:activity"`
+	Nature           int             `gorm:"column:nature"`
+	Science          int             `gorm:"column:science"`
+	National         int             `gorm:"column:national"`
+	Workshop         int             `gorm:"column:workshop"`
+	Creation         int             `gorm:"column:creation"`
+	Kids             int             `gorm:"column:kids"`
+	Tech             int             `gorm:"column:tech"`
+	AboutRussia      int             `gorm:"column:about_russia"`
+	TypeService      string          `gorm:"column:type_service"`
+	Duration         int64           `gorm:"column:duration"`
+	ProcessedPreview string          `gorm:"column:processed_preview"`
+	ProcessedTitle   string          `gorm:"column:processed_title"`
+	WorkSchedule     string          `gorm:"column:work_schedule"`
+	VectorText       pq.Float64Array `gorm:"column:vector_text"`
+	VectorTitle      pq.Float64Array `gorm:"column:vector_title"`
+
+	IsEvent bool `gorm:"column:is_event"`
 }
 
 func (Place) TableName() string {
